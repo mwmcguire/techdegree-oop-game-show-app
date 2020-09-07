fd2269d645f6b679be8793cdb89afe089828c431
mwmcguire <mike@mmcguire.io>
2020-09-07 15:23:08 -0400

    created initial game and phrase classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,42 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missedGuesses = 0;
+    this.phrases = [];
+    this.activePhrase = null;
+  }
+
+  /**
+   * Starts the game.
+   */
+  startGame() {}
+
+  /**
+   * Randomly retrieves one of the phrases stored in the phrases array.
+   * @return  {string}  Random phrase
+   */
+  getRandomPhrase() {}
+
+  /**
+   * Controls game logic.
+   */
+  handleInteraction() {}
+
+  /**
+   * Remove a life from the scoreboard.
+   */
+  removeLife() {}
+
+  /**
+   * Check if player has revealed all letters in active phrase.
+   */
+  checkForWin() {}
+
+  /**
+   *
+   */
+  gameOver() {}
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,24 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+  /**
+   * Adds letter placeholders to the display when game starts.
+   */
+  addPhraseToDisplay() {}
+
+  /**
+   * Checks to see if selected letter matches a letter in the phrase.
+   */
+  checkLetter() {}
+
+  /**
+   * Reveal letter(s) that matches the player's selection.
+   */
+  showMatchedLetter() {}
+}
